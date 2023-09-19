7e86f0f17941523b879b5e8a748f206f9109e51b
Mayson Kotlyar Maksim <143324782+MaksimMyson@users.noreply.github.com>
2023-09-19 16:17:49 +0300

    ex5

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-x = int(input())
-y = int (input())
-if x > y :
-    print(y)
+a = int(input("Число-1"))
+b = int(input("Число-2"))
+choice = int(input())
+if choice == 1:
+    print(a + b)
+elif choice == 2:
+    print(a - b)
+elif choice == 3:
+    print((a + b) / 2)
+elif choice == 4 :
+    print(a * b)
 else:
-    print(x)
+    print("Невірний вибір")
+
+
 
 
 
