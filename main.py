5bb3113356dd2452d4f9a7e5a08b78841dd357ef
Mayson Kotlyar Maksim <143324782+MaksimMyson@users.noreply.github.com>
2023-09-19 17:12:17 +0300

    ex 1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 a = int(input("Число-1"))
 b = int(input("Число-2"))
 c = int(input("Число-3"))
-choice = int(input("Вибiр"))
+print("1.  Сума\n2. Добуток")
+choice = int(input("Ваш вибiр:"))
 if choice == 1:
     print(a + b + c)
 elif choice == 2:
-    print((a  * b * c))
+    print((a  * b * c ))
 else:
     print("Невірний вибір")
 
