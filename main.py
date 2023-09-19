d959bddd7babb03e229e87725a3e72dd2770fe14
Mayson Kotlyar Maksim <143324782+MaksimMyson@users.noreply.github.com>
2023-09-19 17:04:59 +0300

    ex 2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 a = int(input("Число-1"))
 b = int(input("Число-2"))
 c = int(input("Число-3"))
-choice = int(input("Вибiр"))
+print("1.  Вивести максимум\n2. Вивести мiнiмум\n3. Середне арефметичне")
+choice = int(input("Ваш вибiр:"))
 if choice == 1:
-    print(max(a,b,c))
+    print(max(a, b, c))
 elif choice == 2:
-    print(min(a,b,c))
+    print(min(a, b, c))
 elif choice == 3:
     print((a + b + c) / 3)
 else:
