34f727d0932ee4ca843ee01465c345d61c2d8aca
Mayson Kotlyar Maksim <143324782+MaksimMyson@users.noreply.github.com>
2023-09-19 16:38:01 +0300

    ex2

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ b = int(input("Число-2"))
 c = int(input("Число-3"))
 choice = int(input("Вибiр"))
 if choice == 1:
-    print(max(a,b,c))
+    print(a + b + c)
 elif choice == 2:
-    print(min(a,b,c))
-elif choice == 3:
-    print((a + b) / 2)
+    print((a  * b * c))
 else:
     print("Невірний вибір")
 
